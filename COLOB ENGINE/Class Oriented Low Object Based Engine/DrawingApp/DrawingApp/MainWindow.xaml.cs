59c8804a612a05f250bed4bf2809995c4b766bc7
Black-Frog2 <60023004+Black-Frog2@users.noreply.github.com>
2021-02-08 17:52:03 -0600

    dont ask me why its called drawingapp, but we added in a half working main menu system

---
+++ COLOB ENGINE/Class Oriented Low Object Based Engine/DrawingApp/DrawingApp/MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.IO;
+
+namespace DrawingApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ExitGame(object sender, RoutedEventArgs e)
+        {
+           Application.Current.Shutdown();
+        }
+
+        private void Start_Game(object sender, RoutedEventArgs e)
+        {
+           // if (File.Exists(""))
+           // {
+                File.Open("someFile.txt", FileMode.Open);
+           // }
+        }
+    }
+}
---
+++ COLOB ENGINE/Class Oriented Low Object Based Engine/DrawingApp/DrawingApp/name10times/name10times/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace name10times
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //title
+            string title = "My name printed 10 times";
+            Console.Title = title;
+
+
+            //variable for my name
+             string myName = "Name" +
+                "";
+            string space = " ";
+
+
+            //loop for name
+            int i = 1;
+            while(i <= 15)
+            {
+                Console.WriteLine(i.ToString() + ". " + space + myName);
+                Console.ReadKey();
+
+                if (i == 1)
+                    space = null;
+                else if (i == 2)
+                    space = " ";
+                else if (i == 3)
+                    space = "  ";
+
+                else if (i == 4)
+                    space = "   ";
+                else if (i == 5)
+                    space = "    ";
+                else if (i == 6)
+                    space = "      ";
+                else if (i == 7)
+                    space = "       ";
+                else if (i == 8)
+                    space = "        ";
+                else if (i == 9)
+                    space = "         ";
+                else if (i == 10)
+                    space = "        ";
+                else if (i == 11)
+                    space = "       ";
+                else if (i == 12)
+                    space = "      ";
+                else if (i == 13)
+                    space = "     ";
+                else if (i == 14)
+                    space = "   " +
+                        " ";
+                else if (i == 15)
+                    space = "     ";
+
+
+                i++;
+            }
+            Console.ReadKey();
+        }
+
+    }
+}
